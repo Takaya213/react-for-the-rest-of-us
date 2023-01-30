252b3efa43cbb3bd507431bb3b564566f79f5db3
Nicole Lambon <Takaya213@users.noreply.github.com>
2023-01-30 08:40:57 +0200

    Initial Commit

---
+++ app/Main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+import HomeGuest from '../components/HomeGuest'
+
+function Main() {
+    return (
+        <>
+            <Header />
+            <HomeGuest />
+            <Footer />
+        </>
+    )
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#app'))
+root.render(<Main />)
+
+if (module.hot) {
+    module.hot.accept()
+}
