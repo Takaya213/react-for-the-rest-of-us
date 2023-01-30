0cdc11e58a882975bef6814c9ebc43d5e419d009
Nicole Lambon <Takaya213@users.noreply.github.com>
2023-01-30 10:13:05 +0200

    React Router

--- app/Main.js
+++ app/Main.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// Site Layout
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+// Site Structure
 import HomeGuest from '../components/HomeGuest'
+import About from '../components/About'
+import Terms from '../components/Terms'
 
 function Main() {
     return (
-        <>
+        <BrowserRouter>
             <Header />
-            <HomeGuest />
+            <Routes>
+                <Route path='/' element={HomeGuest} />
+                <Route path='/about-us' element={About} />
+                <Route path='/terms' element={Terms} />
+            </Routes>
             <Footer />
-        </>
+        </BrowserRouter>
     )
 }
 
--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer () {
     return(
-        <footer className="border-top text-center small text-muted py-3">
-            <p><a href="/" className="mx-1">Home</a> | <a className="mx-1" href="/about-us">About Us</a> | <a className="mx-1" href="/terms">Terms</a></p>
-            <p className="m-0">Copyright &copy; {new Date().getFullYear()} <a href="/" className="text-muted">ComplexApp</a>. All rights reserved.</p>
+        <footer className='border-top text-center small text-muted py-3'>
+            <p><Link to='/' className='mx-1'>Home</Link> | <Link to='/about-us' className='mx-1'>About Us</Link> | <Link to='/terms' className='mx-1'>Terms</Link></p>
+            <p className='m-0'>Copyright &copy; {new Date().getFullYear()} <Link to='/' className='text-muted'>ComplexApp</Link>. All rights reserved.</p>
         </footer>
     )
 }
